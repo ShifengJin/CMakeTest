8bf0b98722c633844e7590d253d9824c0db29ccc
ShifengJin <15210583738@163.com>
2019-10-09 18:14:12 +0800

    自动查找文件夹下的源文件

--- CMakeListTest/AppMain/main.cpp
+++ CMakeListTest/AppMain/main.cpp
@@ -2,7 +2,7 @@
 #include <TestAdd_lib/TestAdd.h>
 
 int main(int argc, char** argv){
-    int c = TestAdd::add(100, 1);
+    int c = TestAdd::add(100, 11);
     printf("test add result : %d\n", c);
     return 0;
 }
